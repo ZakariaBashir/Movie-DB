f44723537f15ede8a2f930ed5f51495e24dd43ee
zakaria bashir <zakaria.bashir98@gmail.com>
2022-01-21 21:40:05 +0200

    Step_4

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ app.get("/",(req,res)=>{
 app.get("/test", (req, res)=>{
     res.send({status:200, message:"ok"});
 });
-app.get ("/time", (req, res)=>{
+app.get("/time", (req, res)=>{
     const date=new Date();
     res.send({status:200, message:`${date.getHours()}:${date.getSeconds()}` });
 });
+app.get("/hello/:id", (req, res)=>{
+    res.send({status:200, message:`Hello ${req.params.id}`});
+});
+app.get("/hello", (req, res)=>{
+    res.send({status:200, message: `Hello`});
+});
+
+app.get("/search", (req, res)=>{
+    let {s}=req.query;
+    if(s){
+        res.send({status:200, message:"ok", data:s});
+    }else {
+        res.send({status:500, error:true, message:"you have to provide a search"});
+    }});
 
 
 
