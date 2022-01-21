808497ac6d1669d5850aaa8b6c81322f6b18981f
zakaria bashir <zakaria.bashir98@gmail.com>
2022-01-21 22:17:50 +0200

    Step_6

--- index.js
+++ index.js
@@ -49,6 +49,48 @@ app.get("/movies/update", (req, res)=>{});
 app.get("/movies/delete", (req, res)=>{});
 
 
+// Step_6
+app.get("/movies/read/by-date", (req, res)=>{
+    res.send({status:200, data:movies.sort((a,b)=>{
+        return a.year-b.year
+    })});
+});
+
+app.get("/movies/read/by-rating", (req, res)=>{
+    res.send({status:200, data:movies.sort((a,b)=>{
+        return a.rating-b.rating
+    })});
+});
+
+app.get("/movies/read/by-title", (req, res)=>{
+    res.send({status:200, data:movies.sort((a,b)=>{
+        if(a.title<b.title){
+            return -1;
+        }if(a.title>b.title){return 1;}
+        return 0;
+    })});
+});
+/* Second Method:
+const keyMapping = {
+    "by-rating": "rating",
+    "by-title": "title",
+    "by-year": "year"
+}
+
+let orderArrayByKey = (arr, sortBy) => {
+    let orderedArray = [...movies];
+    let key = keyMapping[sortBy];
+
+    orderedArray.sort((a, b) => {
+        if (typeof a[key] === 'string'){
+            if (a[key] > b[key]) return 1;
+            if (a[key] < b[key]) return -1;
+            return 0;
+        }
+        return (a[key] - b[key]);
+    });
+
+    return orderedArray; */
 
 
 
